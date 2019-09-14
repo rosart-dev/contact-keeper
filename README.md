b062225c10a6632f408897594eea36383c9ea298
Emmanuel Rosario <erosario1096@gmail.com>
2019-09-14 11:18:07 -0400

    first commit - complete project

---
+++ README.md
@@ -0,0 +1,5 @@
+# contact-keeper
+
+Full stack MERN Web application with React hooks(Context API, Effects and more), JWT authentication.
+
+Built this project as part of an Udemy course called 'React Front to Back 2019" by Brad Traversy.
---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactItem from "./ContactItem";
+import ContactContext from "../../context/contact/contactContext";
+import Spinner from "../layout/Spinner";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    //eslint-disable-next-line
+  }, []);
+
+  if (contacts && contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {filtered
+            ? filtered.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="contact"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="contact"
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
+};
+
+export default Contacts;
